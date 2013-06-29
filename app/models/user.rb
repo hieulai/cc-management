8366e88ae92b402ef223effbbaf75c638dfe9a10
constructioncentral <lee.jonathanw@gmail.com>
2013-06-29 16:44:31 +0000

    Updated User model for authentication, login, and logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,22 @@
-# require 'digest/SHA1'
+require 'digest/sha1'
 class User < ActiveRecord::Base
  
-
+  #relations
   belongs_to :builder
  
-  attr_accessible :first_name, :last_name, :email, :primary_phone, :password
-
+  #Variables
+  attr_accessible :first_name, :last_name, :email, :primary_phone
+  attr_protected :hashed_password, :salt
+  attr_accessor :password
+  
+  #Validations
+  validates :password, length: {in: 1..20} 
+  
+  #Callbacks
+  before_save :create_hashed_password
+  after_save :clear_password
+  
+  #public methods
   def self.make_salt(email="")
     Digest::SHA1.hexdigest("Use #{email} with #{Time.now} to make salt")
   end
@@ -13,5 +24,32 @@ class User < ActiveRecord::Base
   def self.hash_with_salt(password="", salt="")
     Digest::SHA1.hexdigest("Put #{salt} on the #{password}")
   end
+  
+  def self.authenticate(email="",password="")
+    user = User.find_by_email(email)
+    if user && user.password_match?(password)
+      return user
+    else
+      return false
+    end
+  end
+  
+  def password_match?(password="")
+    hashed_password == User.hash_with_salt(password,salt)
+  end
+  
+  private
+  
+  def create_hashed_password
+    unless password.blank?
+      self.salt = User.make_salt(email) if salt.blank?
+      self.hashed_password = User.hash_with_salt(password, salt)
+    end
+  end
+  
+  def clear_password
+    self.password = nil
+  end
+  
 end
 
