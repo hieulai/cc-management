5288e59772e14bbdc962156b26f6d4475326cc99
Hieu Lai Trung <hieulaitrung@gmail.com>
2013-07-06 21:44:40 +0700

    #2 Task should be reordered

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,8 @@
 class Task < ActiveRecord::Base
 
   belongs_to :tasklist
+  default_scope { order(:position) }
 
-  attr_accessible :name, :tasklist_id, :completed, :time_to_complete, :department
+  attr_accessible :name, :tasklist_id, :completed, :time_to_complete, :department, :position
 
 end
