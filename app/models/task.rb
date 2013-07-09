4b8befacf13e708cb1ef472f8f85fd99472a0c6c
Jonathan W. Lee <lee.jonathanw@gmail.com>
2013-07-08 21:40:54 -0500

    Changed Task ordering to default to ASC

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ActiveRecord::Base
 
   belongs_to :tasklist
-  default_scope { order(:position) }
+  default_scope { order("position ASC") }
 
   attr_accessible :name, :tasklist_id, :completed, :time_to_complete, :department, :position
 
