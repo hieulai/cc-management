addd3405cd1ae6fd50c7fadd591ddb1938367384
Hieu Lai Trung <hieulaitrung@gmail.com>
2013-07-10 08:13:10 +0700

    #12 Importing Items Bug

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,7 @@ class ItemsController < ApplicationController
       format.html
       format.csv {send_data Item.to_csv(@items)}
       # format.xls {send_data Item.to_csv(@builder, col_sep: "\t")}
-      format.xls
+      format.xls { send_data @items.to_xls(:headers => Item::HEADERS, :columns => [:name, :description, :cost, :unit, :margin, :price, :notes]), content_type: 'application/vnd.ms-excel', filename: 'items.xls' }
     end
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,12 +6,14 @@ class Item < ActiveRecord::Base
   has_many :templates, through: :categories_templates_items
   has_and_belongs_to_many :categories_templates
 
-  attr_accessible :name, :description, :qty, :unit, :cost, :margin, :price, :default, :notes, :file
+  attr_accessible :name, :description, :qty, :unit, :cost, :margin, :default, :notes, :file
 
   validates :name, presence: true
   
   scope :search, lambda{|query| where("name LIKE ? OR description LIKE ? OR notes LIKE ?",
      "%#{query}%", "%#{query}%", "%#{query}%")}
+
+  HEADERS = ["name", "description", "cost", "unit", "margin", "price", "notes"]
 
   def price
     if cost && margin
@@ -27,7 +29,7 @@ class Item < ActiveRecord::Base
 
   def self.to_csv(items, options = {})
     CSV.generate(options = {}) do |csv|
-      csv << ["name", "description", "cost", "unit", "margin", "price", "notes"]
+      csv << HEADERS
       items.each do |item|
         csv << [item.name, item.description, item.cost, item.unit, item.margin, item.price, item.notes]
       end
@@ -35,21 +37,12 @@ class Item < ActiveRecord::Base
   end
 
   def self.import(file, builder)
-    CSV.foreach(file.path, headers: true) do |row|
+    spreadsheet = open_spreadsheet(file)
+    header = spreadsheet.row(1)
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
       item = find_by_id(row["id"]) || new
-      item.attributes = row.to_hash.slice("name", "description", "cost", "unit", "margin", "notes")
-      item.builder_id = builder.id
-      item.save!
-    end
-  end
-
-  def self.excel_import(file, builder)
-    Spreadsheet.client_encoding = 'UTF-8'
-    book = Spreadsheet.open file.path
-    sheet1 = book.worksheet 0
-    sheet1.each do |row|
-      item = find_by_id(row["id"]) || new
-      item.attributes = row.to_hash.slice("name", "description", "cost", "unit", "margin", "notes")
+      item.attributes = row.to_hash.slice(*accessible_attributes)
       item.builder_id = builder.id
       item.save!
     end
@@ -57,7 +50,7 @@ class Item < ActiveRecord::Base
 
   def self.open_spreadsheet(file)
     case File.extname(file.original_filename)
-    when ".csv" then CSV.new(file.path, nil)
+    when ".csv" then Roo::Csv.new(file.path,nil)
     when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
     when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
     else raise "Unknown file type: #{file[:data].original_filename}"
