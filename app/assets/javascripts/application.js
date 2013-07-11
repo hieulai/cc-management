afa8169d30a967747d3a27ddd4db1f94ea200ec8
Hieu Lai Trung <hieulaitrung@gmail.com>
2013-07-11 22:10:51 +0700

    #19 Tasklist Bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,13 +37,31 @@ var add_fields = function(link, association, content){
 	$(link).parent().before(content.replace(regexp, new_id));
 }
 
+var load_add_link = function(){
+    if ((addTaskLink = $(".cocoon-container").find("#add_item:hidden")) && ($(".cocoon-container .nested-fields:visible").size() == 0)) {
+        addTaskLink.data("association-insertion-node", 'this')
+            .data("association-insertion-method", 'before')
+            .show()
+    }
+}
+
 $(document).ready(function() {
+    load_add_link();
+
     $(document).on('click', '.trigger_add', function(){
-        $("#" + $(this).data("target")).data("association-insertion-node", '#'+ $(this).closest(".task")[0].id);
-        $("#" + $(this).data("target")).click();
+        var container = $(this).closest(".cocoon-container");
+        container.find("#add_item").data("association-insertion-node", '#' + $(this).closest(".nested-fields")[0].id)
+            .data("association-insertion-method", 'after')
+            .click();
     });
-    $('#tasksContainer').bind('cocoon:after-insert', function (e, task_to_be_added) {
-        task_to_be_added.attr('id', 'task_' + new Date().getMilliseconds());
+    $('.cocoon-container').bind('cocoon:after-insert', function (e, task_to_be_added) {
+        if (addTaskLink = $("#add_item:visible")){
+            addTaskLink.hide()
+        }
+        task_to_be_added.attr('id', 'item_' + new Date().getMilliseconds());
+    });
+    $('.cocoon-container').bind('cocoon:after-remove', function() {
+          load_add_link();
     });
     $( "#tasksContainer" ).sortable({
         revert: true
