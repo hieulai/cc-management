6dbc88020d5398e028c7c34bb8cf885de6ed023b
Hieu Lai Trung <hieulaitrung@gmail.com>
2013-09-11 22:44:45 +0700

    #29 Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,6 @@ Cc::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
    match ':controller(/:action(/:id))(.:format)'
    resources :accounting do
-     get :autocomplete_vendor_trade, :on => :collection
+     get :autocomplete_vendor_name, :on => :collection
    end
 end
