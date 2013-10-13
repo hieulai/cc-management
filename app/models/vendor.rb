037194cf32d531de344a05c8514342937874488f
Hieu Lai Trung <hieulaitrung@gmail.com>
2013-10-14 00:12:00 +0700

    #45 Selecting vendor belongs to session builder

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -15,7 +15,7 @@ class Vendor < ActiveRecord::Base
     (q ? where(["company ILIKE ? or primary_first_name ILIKE ? or primary_last_name ILIKE ? or concat(primary_first_name, ' ', primary_last_name) ILIKE ?", '%'+ q + '%', '%'+ q + '%','%'+ q + '%' ,'%'+ q + '%' ])  : {})
   }
 
-  scope :has_unpaid_bills, lambda { joins(:bills).where("payment_id is null").uniq.all }
+  scope :has_unpaid_bills, lambda { |builder_id| joins(:bills).where("vendors.builder_id= ? and payment_id is null", builder_id).uniq.all }
 
   validates :vendor_type, presence: true
   validates :trade, presence: { message: "cannot be blank for Subcontractors. Consider entering something such as: Framer, Plumber, Electrician, etc."}, if: :vendor_is_subcontractor?
