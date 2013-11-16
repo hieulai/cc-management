d052aa2d9fc49958a5767e307aff36d6a84d5c18
Hieu Lai Trung <hieulaitrung@gmail.com>
2013-11-16 09:51:07 +0700

    #53 Redirect accounting index to receivables

--- app/controllers/accounting_controller.rb
+++ app/controllers/accounting_controller.rb
@@ -2,7 +2,7 @@ class AccountingController < ApplicationController
   before_filter :authenticate_user!
   
   def index
-    render('receivables')
+    redirect_to(:action => 'receivables')
   end
 
   def receivables
