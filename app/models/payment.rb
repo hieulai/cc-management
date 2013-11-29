148f53796f06074169c39a627381d873bbefc9ab
Hieu Lai Trung <hieulaitrung@gmail.com>
2013-11-29 11:35:08 +0700

    #54 Fix bug calculating account amount

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -15,6 +15,8 @@ class Payment < ActiveRecord::Base
 
   validates_presence_of :vendor, :account, :builder, :method, :date
 
+  after_update :update_account_balance, :if => :account_id_changed?
+
   METHODS = ["Check", "Debit Card", "Wire", "EFT"]
 
   def amount
@@ -23,6 +25,15 @@ class Payment < ActiveRecord::Base
 
   def payee
     vendor.try(:display_name)
+  end
+
+  def update_account_balance
+    old_account = Account.find(account_id_was)
+    account = Account.find(account_id)
+    payments_bills.each do |pb|
+      old_account.update_attribute(:balance, old_account.balance + pb.amount)
+      account.update_attribute(:balance, account.balance - pb.amount)
+    end
   end
 
 end
