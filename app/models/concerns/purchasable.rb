5db5fd6f92b2a5e4bc2ad6cc2a1801fef43a874d
Hieu Lai Trung <hieulaitrung@gmail.com>
2013-12-29 11:00:08 +0700

    #84 Improve actual_cost calculation

--- app/models/concerns/purchasable.rb
+++ app/models/concerns/purchasable.rb
@@ -5,20 +5,14 @@ module Purchasable
     belongs_to :item
 
     attr_accessible :description, :qty, :estimated_cost, :actual_cost, :item_id
-    before_save :unset_actual_costs, :set_actual_costs
-    before_destroy :unset_actual_costs
+    before_save :compare_item_costs
 
-    def set_actual_costs
-      updated_cost = self.item.actual_cost.to_f + self.actual_cost.to_f
-      unless self.item.update_attribute(:actual_cost, updated_cost)
-        errors[:base] << item.errors.full_messages.join(".")
+    def compare_item_costs
+      update_cost = item.actual_cost.to_f + actual_cost.to_f - actual_cost_was.to_f
+      if item.committed_cost.present? && (update_cost > item.committed_cost.to_f)
+        errors[:base] << "Total paid amount of item #{item.name}: $#{update_cost} is greater than total bid amount: $#{item.committed_cost}"
         return false
       end
-    end
-
-    def unset_actual_costs
-      updated_cost = self.item.actual_cost.to_f - self.actual_cost_was.to_f
-      self.item.update_column(:actual_cost, updated_cost == 0 ? nil : updated_cost)
     end
   end
 
