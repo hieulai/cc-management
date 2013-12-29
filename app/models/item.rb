b70c0c61d6a0ccde62b49e1a10e5d892e8a31123
Hieu Lai Trung <hieulaitrung@gmail.com>
2013-12-29 10:23:59 +0700

    #84 Fix total calculation

--- app/models/item.rb
+++ app/models/item.rb
@@ -50,7 +50,7 @@ class Item < ActiveRecord::Base
   def actual_cost
     if bills_items.any? || purchase_orders_items.any?
       bills_items.sum(&:actual_cost).to_f + purchase_orders_items.sum(&:actual_cost).to_f
-    else
+    elsif bill || purchase_order
       read_attribute(:actual_cost)
     end
   end
