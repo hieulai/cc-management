f1a424425a52dfd99be47e50b6e5a1c096fa3e38
Viet Nguyen <vietnt84@gmail.com>
2013-12-31 00:40:26 +0700

    #57: Back-End Design Implementation: Reduce overall height of the header/menu area

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -120,7 +120,7 @@ $(document).ready(function() {
             "aaSorting": [[ 0, "desc" ]]
         }
         if ($(this).closest("#payables").size() > 0 ||  $(this).closest("#receivables").size()>0){
-            options.sScrollY = "7.8em";
+            options.sScrollY = "8.7em";
         }
         $(this).dataTable(options);
         $("#" + $(this).attr("id") + "_wrapper").prev(".button-group").appendTo("#" + $(this).attr("id") + "_filter");
