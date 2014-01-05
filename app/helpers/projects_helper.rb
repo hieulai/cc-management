306ae30da6e62479d1e2145746c182b3714dae0c
Hieu Lai Trung <hieulaitrung@gmail.com>
2014-01-05 16:34:12 +0700

    Improvement for projects have no estimate

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -3,7 +3,7 @@ module ProjectsHelper
     return [] unless project
     result = []
     original_categories = project.estimates.any? ? project.estimates.first.template.categories_templates.sort_by { |c| c.category.name }.map { |c| c.category } : Array.new
-    co_categories = project.cos_categories.any? ? project.cos_categories.sort_by { |c| c.name } : Array.new
+    co_categories = project.estimates.any? && project.cos_categories.any? ? project.cos_categories.sort_by { |c| c.name } : Array.new
     result << ['From estimate', original_categories.map { |c| [c.name, c.id] }] if original_categories.any?
     result << ['From change orders', co_categories.map { |c| [c.name, c.id] }] if co_categories.any?
     result
