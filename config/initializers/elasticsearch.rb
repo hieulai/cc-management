27c5adabdff2ef4ece7a75752836bc69e960471b
Hieu Lai Trung <hieulaitrung@gmail.com>
2014-03-06 00:42:11 +0700

    Add ELASTICSEARCH_URL

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,4 +1,4 @@
 # Connect to specific Elasticsearch cluster
-ELASTICSEARCH_URL = ENV['BONSAI_URL'] || 'http://localhost:9200'
+ELASTICSEARCH_URL = ENV['ELASTICSEARCH_URL'] || 'http://localhost:9200'
 
 Elasticsearch::Model.client = Elasticsearch::Client.new ({host: ELASTICSEARCH_URL})
