08fe0c987c98d7566b8ad5639182290a6262c971
Hieu Lai Trung <hieulaitrung@gmail.com>
2014-04-10 22:40:32 +0700

    # 169 Payments Accept All Payors

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -14,7 +14,7 @@ class Vendor < ActiveRecord::Base
     (q ? where(["company ILIKE ? or primary_first_name ILIKE ? or primary_last_name ILIKE ? or concat(primary_first_name, ' ', primary_last_name) ILIKE ?", '%'+ q + '%', '%'+ q + '%','%'+ q + '%' ,'%'+ q + '%' ])  : {})
   }
 
-  scope :has_unpaid_bills, joins(:bills).where("bills.remaining_amount is NULL OR bills.remaining_amount > 0").uniq.all
+  scope :has_unpaid_bills, joins(:bills).where("bills.remaining_amount is NULL OR bills.remaining_amount > 0").uniq
 
   validates :vendor_type, presence: true
   validates :trade, presence: { message: "cannot be blank for Subcontractors. Consider entering something such as: Framer, Plumber, Electrician, etc."}, if: :vendor_is_subcontractor?
