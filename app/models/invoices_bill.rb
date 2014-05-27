97506861b7b8412ecfec101ff3732ede9d2b2d9f
Hieu Lai Trung <hieulaitrung@gmail.com>
2014-05-27 09:15:20 +0700

    #180 Cost+ Invoicing Bug

--- app/models/invoices_bill.rb
+++ app/models/invoices_bill.rb
@@ -13,13 +13,17 @@ class InvoicesBill < ActiveRecord::Base
   after_destroy :refund_invoices_accounts
 
   def update_invoices_accounts
-    revenue_ia = InvoicesAccount.where(invoice_id: invoice.id, account_id: bill.categories_template.revenue_account.id).first_or_create
-    revenue_ia.update_attributes({date: invoice.date, amount: revenue_ia.amount.to_f + amount.to_f})
+    bill.categories_templates.each do |ct|
+      revenue_ia = InvoicesAccount.where(invoice_id: invoice.id, account_id: ct.revenue_account.id).first_or_create
+      revenue_ia.update_attributes({date: invoice.date, amount: revenue_ia.amount.to_f + amount.to_f})
+    end
   end
 
   def refund_invoices_accounts
-    revenue_ia = InvoicesAccount.where(invoice_id: invoice.id, account_id: bill.categories_template.revenue_account.id).first_or_create
-    revenue_ia.update_attributes({date: invoice.date, amount: revenue_ia.amount.to_f - amount_was.to_f})
-    revenue_ia.destroy if bill.categories_template.revenue_account.invoices_bills.where(invoice_id: invoice_id).empty?
+    bill.categories_templates.each do |ct|
+      revenue_ia = InvoicesAccount.where(invoice_id: invoice.id, account_id: ct.revenue_account.id).first_or_create
+      revenue_ia.update_attributes({date: invoice.date, amount: revenue_ia.amount.to_f - amount_was.to_f})
+      revenue_ia.destroy if ct.revenue_account.invoices_bills.where(invoice_id: invoice_id).empty?
+    end
   end
 end
