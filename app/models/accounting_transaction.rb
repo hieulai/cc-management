f9f0766a62eb24cdb52e85f8063c164f9ee204f9
Hieu Lai Trung <hieulaitrung@gmail.com>
2014-06-14 12:37:38 +0700

    Fix Account Ledger Bug

--- app/models/accounting_transaction.rb
+++ app/models/accounting_transaction.rb
@@ -4,7 +4,7 @@ class AccountingTransaction < ActiveRecord::Base
 
   attr_accessible :transactionable_id, :transactionable_type, :date, :amount, :reconciled, :account_id, :display_priority
 
-  default_scope order("date DESC NULLS LAST, display_priority DESC")
+  default_scope order("date DESC NULLS LAST, display_priority DESC, created_at DESC")
   scope :unreconciled, where(:reconciled => false)
   scope :reconciled, where(:reconciled => true)
   scope :date_range, lambda { |from_date, to_date| where('date >= ? AND date <= ?', from_date, to_date) }
