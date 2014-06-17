98473352516592db5ad8a8bb22dcb33dfcd09ea3
Hieu Lai Trung <hieulaitrung@gmail.com>
2014-06-18 00:32:43 +0700

    # 129 Improve active menu highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,25 +1,4 @@
 module ApplicationHelper
-  def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
-  end
-
-  def link_to_add_fields(name, f, association)
-    new_object = f.object.class.reflect_on_association(association).klass.new
-    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-      render(association.to_s.singularize + "_fields", :f => builder)
-    end
-    link_to_function(name, h("add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")"))
-  end
-
-  def active_page(page_name, menu_type, act_name = nil)
-    if page_name.include?(controller_name) && act_name.nil?
-      'active'
-    elsif (controller_name.eql?(page_name)) && action_name.eql?(act_name)
-      'active'
-    else
-      ''
-    end
-  end
 
   def active_nav(page_name, c_name= nil, act_name = nil)
     root_path = request.path.split('/')[1]
