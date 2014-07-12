82ccb3a82271970574bd48320c65c0a814108a8e
Hieu Lai Trung <hieulaitrung@gmail.com>
2014-07-12 23:12:21 +0700

    #199 Tweak migration script

--- db/migrate/20140629164115_add_committed_to_estimates.rb
+++ db/migrate/20140629164115_add_committed_to_estimates.rb
@@ -2,7 +2,7 @@ class AddCommittedToEstimates < ActiveRecord::Migration
   def change
     add_column :estimates, :committed, :boolean
 
-    Project.where(status: [Project::CURRENT, Project::PAST]).joins(:estimates).each do |p|
+    Project.where(status: [Project::CURRENT, Project::PAST]).has_estimate.each do |p|
       p.estimates.first.update_attribute(:committed, true)
     end
   end
