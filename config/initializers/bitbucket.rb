ad01eb0998eed6fdb96df6babfea96326c8e0263
Hieu Lai Trung <hieulaitrung@gmail.com>
2014-07-29 00:28:36 +0700

    #181 Adjust BitBucket configurations

--- config/initializers/bitbucket.rb
+++ config/initializers/bitbucket.rb
@@ -1,6 +1,6 @@
 BitBucket.configure do |config|
   config.client_id     = 'JYUf3MNGeqjuqaN3Rh'
   config.client_secret = 'uuARG5yQuzKjKRDfafsSwZEmAfqGXke4'
-  config.repo = 'Construction Central'
-  config.user = 'hieulaitrung'
+  config.repo = 'construction-central'
+  config.user = 'jonathanwlee'
 end
